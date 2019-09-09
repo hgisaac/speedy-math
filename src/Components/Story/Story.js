20f6410afd81472c03c95af0f6f1aa844f9379b1
Hugo Isaac <hugo_iar@live.com>
2019-09-09 00:38:23 -0300

    Created story component

---
+++ src/Components/Story/Story.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Story = (prop) => {
+  return (
+    <div></div>
+  )
+}
+
+Story.propTypes = {
+  storyState: PropTypes.string
+}
+
+export default Story;
