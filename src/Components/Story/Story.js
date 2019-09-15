8ad191fdd1b540a4863a0d0df7e9896f53a3d7c6
Hugo Isaac <hugo_iar@live.com>
2019-09-15 00:32:40 -0300

    Updated story component

--- src/Components/Story/Story.js
+++ src/Components/Story/Story.js
@@ -1,13 +1,38 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import StoryText from './Story.json';
 
 const Story = (prop) => {
+  function handleStory() {
+    let story
+    switch (prop.storyState) {
+      case "+":
+        story = StoryText.addStory
+        break;
+      case "-":
+        story = StoryText.subStory
+        break;
+      case "x":
+        story = StoryText.multiStory
+        break;
+      case "___":
+        story = StoryText.divStory
+        break;
+      default:
+        story = StoryText.onboard
+    }
+    return story
+  }
+
   return (
-    <div className="Story__Container">Hello</div>
+    <div className="Story__Container">
+      {handleStory()}
+    </div>
   )
 }
 
 Story.propTypes = {
+  onboard: PropTypes.bool,
   storyState: PropTypes.string
 }
 
