be2638085dceb0791b7e4d3285a63602b9ac69c5
Hugo Isaac <hugo_iar@live.com>
2019-09-17 00:58:51 -0300

    Updated story component

--- src/Components/Story/Story.js
+++ src/Components/Story/Story.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import StoryText from './Story.json';
+import StoryText from './StoryText.json';
 import './Story.css';
 
 const Story = (prop) => {
